5179d8444e2bb8f9be3257a174c46572c9d5d13d
Mahak chhipa <mehekjajpuraom700@gmail.com>
2024-09-06 17:19:00 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,31 @@
+console.log("Shree Ganesh");
+Fullname ="MAHAK CHHIPA";
+console.log(Fullname);
+Age=("22");
+console.log(Age);
+console.log(`${document.URL}`);
+alert(document.URL);
+// Define a function named diff_num that takes a parameter n
+function diff_num(n) {
+    // Check if n is less than or equal to 19
+    if (n <= 19) {
+      // If true, return the difference between 19 and n
+      return (19 - n);
+    } else {
+      // If false, return three times the difference between n and 19
+      return (n - 19) * 3;
+    }
+  }
+  
+  // Log the result of calling the diff_num function with the argument 12 to the console
+  console.log(diff_num(1));
+  
+  // Log the result of calling the diff_num function with the argument 19 to the console
+  console.log(diff_num(15));
+  
+  // Log the result of calling the diff_num function with the argument 22 to the console
+  console.log(diff_num(22)); 
+  const fruits= [ "banana" , "papaya", "fig","grapes","orange"];
+  const list = fruits.entries();
+  console.log(list);
+  
